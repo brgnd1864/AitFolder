40620673e1a9b5307d0376e182530097ff3ff872
7666 <7666@ait-sydney.net.au>
2021-06-18 11:17:07 +1000

    first-commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//
+/*\*/
+
+$name= "Bryan";
+$day="Friday";
+$data = 18;
+$test="Hello PHP";
+//concatenation of strings
+
+echo "Hello, ". $name ."Today is ".$day;
+// output html with double quotes
+echo "<h1>$data</h1>";
+//output html wutg suge quotes
+echo '<p>' . $day.'</p>';
+
+?>
+<div style="bacground-color:green;color:white;">
+<?php echo $test?>
+</div>
